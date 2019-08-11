2795a1eec983612c3b9f81bef801dd05b4c73d8c
Lih <alinecsdiogo@gmail.com>
2019-08-10 21:19:31 -0300

    readme

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -2,11 +2,10 @@ const axios = require('axios')
 const Dev = require('../models/Dev')
 
 module.exports = {
+
     async index(req, res) {
         const { user } = req.headers
-
         const loggedDev = await Dev.findById(user)
-
         const users = await Dev.find({
             $and: [
                 { _id: { $ne: user } },
@@ -19,7 +18,6 @@ module.exports = {
 
     async  store(req, res) {
         const { username } = req.body
-
         const userExists = await Dev.findOne({ user: username })
 
         if (userExists) {
@@ -27,9 +25,7 @@ module.exports = {
         }
 
         const response = await axios.get(`https://api.github.com/users/${username}`)
-
         const { name, bio, avatar_url: avatar } = response.data
-
         const dev = await Dev.create({
             name,
             user: username,
